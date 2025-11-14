c3c1444f35f3d3e7ef80c190402ecd20b7abd1f1
lumialfe <lumialfe@hotmail.com>
2025-11-14 12:01:46 +0100

    config: add gh pages configuration to next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig: NextConfig = {
-  /* config options here */
+output: 'export',           // Enables static export
+basePath: isProd ? '/curriculum-vitae' : '',
+assetPrefix: isProd ? '/curriculum-vitae/' : '',
 };
 
 export default nextConfig;
