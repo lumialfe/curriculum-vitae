80300600c7ed59076ef15db0978f9624d203ce76
lumialfe <lumialfe@hotmail.com>
2025-11-14 12:15:18 +0100

    config: update deployment config

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig: NextConfig = {
-output: 'export',           // Enables static export
-basePath: isProd ? '/curriculum-vitae' : '',
-assetPrefix: isProd ? '/curriculum-vitae/' : '',
+		output: 'export',           // Enables static export
+		basePath: isProd ? '/curriculum-vitae' : '',
+		assetPrefix: isProd ? '/curriculum-vitae/' : '',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
