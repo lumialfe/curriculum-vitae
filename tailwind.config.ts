5135393bdfa2325c1f0217b8bcb94549d233f617
lumialfe <lumialfe@hotmail.com>
2025-11-17 09:16:18 +0100

    config: configure tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: ['./{src,.storybook}/**/*.{js,jsx,ts,tsx,mdx}'],
+  theme: {},
+  plugins: [],
+}
+
+export default config
