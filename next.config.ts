a84088d012dbb3786e4564e36446699e70fdc0ed
LuisMi Alonso <60442261+lumialfe@users.noreply.github.com>
2025-11-17 09:33:55 +0100

    config: Add storybook (#6)

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig: NextConfig = {
-		output: 'export',           // Enables static export
+		output: 'export',
 		basePath: isProd ? '/curriculum-vitae' : '',
 		assetPrefix: isProd ? '/curriculum-vitae/' : '',
 };
